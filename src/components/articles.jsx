41674e8113574eb148d56b860ef1413b9e7b272a
Thammaruthai <thammaruthai@gmail.com>
2024-10-23 14:31:40 +0700

    feat: add react State for Tab in desktop and Select on moible sizing

--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,10 +1,11 @@
+//Import Zone
 import { Input } from "@/components/ui/input";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Search } from "lucide-react";
 import * as React from "react";
 import { blogPosts } from "../data/blogPosts";
 import authorImage from "../img/profilePic.jpg";
-
+import { useState } from "react";
 import {
   Select,
   SelectContent,
@@ -16,26 +17,37 @@ import {
 } from "@/components/ui/select";
 
 export function ArticlesSection() {
+  //declare zone
   const categories = ["Highlight", "Cat", "Inspiration", "General"];
+  const [selectCategory, setSelectCategory] = useState(categories[0]);
 
+  //Function zone
+  
+
+  //Html zone
   return (
     <>
+      {/* Last Article */}
       <div className="flex flex-row w-full bg-[background: #EFEEEB;] justify-center h-16 items-center">
         <div className="w-11/12">
-          <div className="font-semibold text-2xl">Latest articles</div>
+          <div className="font-semibold text-2xl">
+            Latest articles
+          </div>
         </div>
       </div>
 
+      {/* Search and select categories - show when Desktop size reponsive*/}
       <div className="flex flex-row w-full bg-[background: #EFEEEB;] justify-center h-16">
-        <div className="flex flex-row justify-between bg-[#EFEEEB] w-11/12 h-20 items-center min-sm:rounded-2xl px-4 max-sm:w-full max-sm:flex-col items-center max-sm:py-16 max-sm:justify-center max-sm:gap-2">
+        <div className="flex flex-row justify-between bg-[#EFEEEB] w-11/12 h-20 items-center sm:rounded-2xl px-4 max-sm:w-full max-sm:flex-col items-center max-sm:py-16 max-sm:justify-center max-sm:gap-2">
           <div id="tab" className="max-sm:hidden">
-            <Tabs defaultValue="Highlight" className="w-[400px]">
+            <Tabs defaultValue={selectCategory} className="w-[400px]">
               <TabsList className="grid w-full grid-cols-4 rounded-md bg-[#EFEEEB]">
                 {categories.map((category) => (
                   <TabsTrigger
                     key={category}
                     value={category}
-                    className="rounded-sm data-[state=active]:bg-[#DAD6D1] data-[state=active]:text-[#43403B]"
+                    className="rounded-sm data-[state=active]:bg-[#DAD6D1] data-[state=active]:text-[#43403B] hover:bg-gray-100  transition-colors"
+                    onClick={() => setSelectCategory(category)}
                   >
                     {category}
                   </TabsTrigger>
@@ -44,6 +56,7 @@ export function ArticlesSection() {
             </Tabs>
           </div>
 
+          {/* Search and select categories - All reponsive*/}
           <div className="relative w-80 max-sm:w-full">
             <Input
               type="text"
@@ -53,16 +66,21 @@ export function ArticlesSection() {
             <Search className="absolute right-3 top-1/2 transform -translate-y-1/2 h-4 w-4 text-gray-400" />
           </div>
 
+          {/* Search and select categories - show when sm size reponsive*/}
           <div id="selectMenu" className="sm:hidden w-full text-[#75716B]">
             <div className="text-base">Category</div>
-            <Select>
+            <Select onValueChange={(value) => setSelectCategory(value)}>
               <SelectTrigger className="w-full bg-white">
-                <SelectValue placeholder="Highlight" />
+                <SelectValue placeholder={selectCategory} />
               </SelectTrigger>
               <SelectContent className="bg-white">
                 <SelectGroup>
                   {categories.map((category) => (
-                    <SelectItem key={category} value={category}>
+                    <SelectItem
+                      key={category}
+                      value={category}
+                      className="hover:bg-gray-100 hover:text-gray-900 transition-colors"
+                    >
                       {category}
                     </SelectItem>
                   ))}
@@ -73,18 +91,20 @@ export function ArticlesSection() {
         </div>
       </div>
 
+      {/* Blog card - */}
       <div className="w-full flex flex-row justify-center sm:mt-16 max-sm:mt-20">
         <div className="w-11/12">
           <article className="grid grid-cols-1 md:grid-cols-2 gap-8 px-4 md:px-0">
             {blogPosts.map((post, index) => (
               <BlogCard
-                key={index} 
+                key={index}
                 image={post.image}
                 category={post.category}
                 title={post.title}
                 description={post.description}
                 author={post.author}
                 date={post.date}
+                EnableCat={selectCategory}
               />
             ))}
           </article>
@@ -94,10 +114,27 @@ export function ArticlesSection() {
   );
 }
 
-function BlogCard({ image, category, title, description, author, date }) {
+export function BlogCard({
+  image,
+  category,
+  title,
+  description,
+  author,
+  date,
+  EnableCat,
+}) {
   return (
-    <div className="flex flex-col gap-4">
-      <a href="#" className="relative h-[212px] sm:h-[360px]">
+    <div
+      className={`flex flex-col gap-4 transition-transform ${
+        EnableCat === category ? "hover:scale-105" : ""
+      }`}
+    >
+      <a
+        href={EnableCat === category ? "#" : undefined}
+        className={`relative h-[212px] sm:h-[360px] ${
+          EnableCat !== category ? "pointer-events-none cursor-default" : ""
+        }`}
+      >
         <img
           className="w-full h-full object-cover rounded-md"
           src={image}
@@ -112,7 +149,7 @@ function BlogCard({ image, category, title, description, author, date }) {
           </span>
         </div>
 
-        <a href="#">
+        <a href={EnableCat === category ? "#" : undefined}>
           <h2 className="font-bold text-xl mb-2 line-clamp-2 hover:underline">
             {title}
           </h2>
